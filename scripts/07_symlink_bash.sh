0afd847a7b1007e5d67ab9016c6d6a09c87ea4e6
Chris <chrisbreuer93@gmail.com>
2021-09-13 00:53:16 -0700

    chore: wip

--- scripts/07_symlink_bash.sh
+++ scripts/07_symlink_bash.sh
@@ -8,10 +8,7 @@
 
 cd $HOME/cardano-helpers
 
-sudo ln -s ./config/.bash_aliases $HOME/.bash_aliases
+sudo ln -s $HOME/cardano-helpers/config/.bashrc $HOME/.bashrc
+sudo ln -s $HOME/cardano-helpers/config/.bash_aliases $HOME/.bash_aliases
 
-cat > $HOME/.bashrc << EOF
-if [ -e $HOME/.bash_aliases ]; then
-    source $HOME/.bash_aliases
-fi
-EOF
+eval "$(cat $HOME/.bashrc | tail -n +10)"
