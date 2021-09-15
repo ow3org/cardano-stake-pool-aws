fbd185c98e4762741f0e1c6a6ae3773e9649ff1b
Chris <chrisbreuer93@gmail.com>
2021-09-15 15:16:34 -0700

    chore: wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,12 +9,13 @@ start=`date +%s.%N`
 banner="--------------------------------------------------------------------------"
 
 $HOME/cardano-helpers/scripts/01_install_server_dependencies.sh
-$HOME/cardano-helpers/scripts/02_install_libsodium.sh
-$HOME/cardano-helpers/scripts/03_install_cabal_and_dependencies.sh
-$HOME/cardano-helpers/scripts/04_build_node_and_configure.sh
+$HOME/cardano-helpers/scripts/02_update_ssh_port.sh
+$HOME/cardano-helpers/scripts/03_install_libsodium
+$HOME/cardano-helpers/scripts/04_install_cabal_and_dependencies.sh
 $HOME/cardano-helpers/scripts/05_create_startup_scripts.sh
-$HOME/cardano-helpers/scripts/06_install_gLiveView_monitoring_tool.sh
-$HOME/cardano-helpers/scripts/07_symlink_bash.sh
+$HOME/cardano-helpers/scripts/06_build_node_and_configure.sh
+$HOME/cardano-helpers/scripts/07_install_gLiveView_monitoring_tool.sh
+$HOME/cardano-helpers/scripts/08_symlink_bash.sh
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l ) || true
