b58a04e47ef2591b6d870a4faf7590c85a76ea26
Chris <chrisbreuer93@gmail.com>
2021-09-28 11:17:05 -0700

    chore: wip

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -6,6 +6,17 @@
 
 # Starts the node.
 
+##
+if [[ -S "${CARDANO_NODE_SOCKET_PATH}" ]]; then
+  if pgrep -f "[c]ardano-node.*.${CARDANO_NODE_SOCKET_PATH}"; then
+     echo "ERROR: A Cardano node is already running, please terminate this node before starting a new one with this script. You may run the command `restart`"
+     exit 1
+  else
+    echo "WARN: A prior running Cardano node was not properly shutdown. The socket file still exists. Cleaning it up..."
+    unlink "${CARDANO_NODE_SOCKET_PATH}"
+  fi
+fi
+
 DIRECTORY=$NODE_HOME
 PORT=6000
 HOSTADDR=0.0.0.0
@@ -13,11 +24,11 @@ TOPOLOGY=${DIRECTORY}/${NODE_CONFIG}-topology.json
 DB_PATH=${DIRECTORY}/db
 SOCKET_PATH=${DIRECTORY}/db/socket
 CONFIG=${DIRECTORY}/${NODE_CONFIG}-config.json
-KES=\${DIRECTORY}/kes.skey
-VRF=\${DIRECTORY}/vrf.skey
-CERT=\${DIRECTORY}/node.cert
+KES=${DIRECTORY}/kes.skey
+VRF=${DIRECTORY}/vrf.skey
+CERT=${DIRECTORY}/node.cert
 
-if [[ -f "${VRF}" && -f "${VRF}" && -f "${CERT}" ]]; then
+if [[ -f "${KES}" && -f "${VRF}" && -f "${CERT}" ]]; then
   # start the block producing node
   /usr/local/bin/cardano-node run \
     --topology "${TOPOLOGY}" \
