94e490e3aa252f44520442989bf527bca256c12c
Chris <chrisbreuer93@gmail.com>
2021-09-30 12:53:13 -0700

    chore: wip

--- build/09_install_monitoring_tools.sh
+++ build/09_install_monitoring_tools.sh
@@ -15,8 +15,12 @@ eval "$(cat $HELPERS/config/.bashrc | tail -n +10)"
 cd $NODE_HOME
 sudo apt install bc tcptraceroute -y
 curl -s -o gLiveView.sh https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/gLiveView.sh
-curl -s -o env https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/env
 chmod 755 gLiveView.sh
+
+curl -s -o env https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/env
+sed -i env \
+     -e "s/\#CONFIG=\"\${CNODE_HOME}\/files\/config.json\"/CONFIG=\"\${NODE_HOME}\/\${NODE_CONFIG}-config.json\"/g" \
+     -e "s/\#SOCKET=\"\${CNODE_HOME}\/sockets\/node0.socket\"/SOCKET=\"\${NODE_HOME}\/db\/socket\"/g"
 
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l ) || true
