ed48c208f6c55241338ae95f8b8264484239aaee
Chris <chrisbreuer93@gmail.com>
2021-10-03 15:02:31 -0700

    chore: wip

--- scripts/updates/cardano-node_1.30.1.sh
+++ scripts/updates/cardano-node_1.30.1.sh
@@ -12,6 +12,32 @@ banner="------------------------------------------------------------------------
 
 eval "$(cat $HELPERS/config/.bashrc | tail -n +10)"
 
+export BOOTSTRAP_HASKELL_NONINTERACTIVE=true
+export BOOTSTRAP_HASKELL_GHC_VERSION=8.10.4
+export BOOTSTRAP_HASKELL_CABAL_VERSION=3.4.0.0
+
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
+GHCUP_PROFILE_FILE="/home/ubuntu/.bashrc"
+GHCUP_DIR="/home/ubuntu/.ghcup"
+echo "[ -f \"${GHCUP_DIR}/env\" ] && source \"${GHCUP_DIR}/env\" # ghcup-env" >> "${GHCUP_PROFILE_FILE}"
+
+eval "$(cat $HELPERS/config/.bashrc | tail -n +10)"
+
+ghcup upgrade
+
+echo "Installing GHC v${BOOTSTRAP_HASKELL_GHC_VERSION} ..."
+ghcup install ghc ${BOOTSTRAP_HASKELL_GHC_VERSION}
+ghcup set ghc ${BOOTSTRAP_HASKELL_GHC_VERSION}
+
+echo "Installing Cabal v${BOOTSTRAP_HASKELL_CABAL_VERSION}.."
+ghcup install cabal ${BOOTSTRAP_HASKELL_CABAL_VERSION}
+ghcup set cabal ${BOOTSTRAP_HASKELL_CABAL_VERSION}
+
+eval "$(cat $HELPERS/config/.bashrc | tail -n +10)"
+
+cabal update
+
 mkdir -p ${HOME}/git
 cd ${HOME}/git || error
 mv -vf ${HOME}/git/cardano-node ${HOME}/git/cardano-node-old || true
