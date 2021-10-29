946f1e83f13289b7a8218d164379eff868e8440d
Chris <chrisbreuer93@gmail.com>
2021-10-29 14:23:05 -0700

    chore: wip

--- build/01_configure_bash.sh
+++ build/01_configure_bash.sh
@@ -12,21 +12,19 @@ banner="------------------------------------------------------------------------
 
 eval "$(cat /home/ubuntu/cardano-stake-pool-helpers/config/.bashrc | tail -n +10)"
 
-# echo NODE_BUILD_NUM=$(curl https://hydra.iohk.io/job/Cardano/iohk-nix/cardano-deployment/latest-finished/download/1/index.html | grep -e "build" | sed 's/.*build\/\([0-9]*\)\/download.*/\1/g') >> $HOME/.bashrc
+# symlink the .bashrc file
+ln -sf ~/cardano-stake-pool-helpers/config/.bashrc ~/.bashrc\
+cp ~/cardano-stake-pool-helpers/.env.example ~/.node-config
 
-# if [ "$NODE_CONFIG" = "mainnet" ]; then
-#     sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT=--mainnet/g"
-# elif [ "$NODE_CONFIG" = "testnet" ]; then
-#     sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT='--testnet-magic 1097911063'/g"
-# # elif [ "$NODE_CONFIG" = "guild" ]; then
-# #     sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT=--guild/g"
-# # elif [ "$NODE_CONFIG" = "staging" ]; then
-# #     sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT=--staging/g"
-# fi
-
-# symlink a few config files - overwrite if they already exist
-ln -sf ~/cardano-stake-pool-helpers/config/.bashrc ~/.bashrc
-ln -sf ~/cardano-stake-pool-helpers/config/.node-config ~/.node-config
+if [ "$NODE_CONFIG" = "mainnet" ]; then
+    sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT=--mainnet/g"
+elif [ "$NODE_CONFIG" = "testnet" ]; then
+    sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT='--testnet-magic 1097911063'/g"
+# elif [ "$NODE_CONFIG" = "guild" ]; then
+#     sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT=--guild/g"
+# elif [ "$NODE_CONFIG" = "staging" ]; then
+#     sed -i ./.node-config -e "s/NETWORK_ARGUMENT=/NETWORK_ARGUMENT=--staging/g"
+fi
 
 eval "$(cat /home/ubuntu/cardano-stake-pool-helpers/config/.bashrc | tail -n +10)"
 
